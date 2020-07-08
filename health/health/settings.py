712db125ab6d8eabb7abfdfcbeb3c0d048015cb0
Palash A <pa334@cornell.edu>
2020-07-07 22:09:57 -0400

    postgres run; api app start

--- health/health/settings.py
+++ health/health/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'health.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'djangoadmin',
+        'PASSWORD': 'django307',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
